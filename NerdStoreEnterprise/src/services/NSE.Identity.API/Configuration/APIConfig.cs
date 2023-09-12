6aa709468c0623409c043cf90461a568eabea588
pmfrp <pmfrp@hotmail.com>
2023-09-12 17:01:26 -0300

    Refatoracao da API Identity

---
+++ NerdStoreEnterprise/src/services/NSE.Identity.API/Configuration/APIConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace NSE.Identity.API.Configuration
+{
+    public static class APIConfig
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services) {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+
+            return services;
+        }
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment environment) {
+            app.UseISwaggerConfiguration(environment);
+
+            app.UseHttpsRedirection();
+
+            app.UseIdentityConfiguration();
+
+            return app;
+        }
+    }
+}
