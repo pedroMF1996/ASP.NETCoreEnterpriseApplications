d226ae513654bad035f71911108bde24a0921701
pmfrp <pmfrp@hotmail.com>
2023-09-14 17:51:30 -0300

    Paginas de login e registro Autenticacao com cookie na aplicacao Controller de Identidade

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void AddWebAppConfig(this IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public static void UseWebAppConfig(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (!environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseIdentityConfig();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro() {
+            ViewBag.Title = "Nova conta";
+            return View(); 
+        }
+        
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(RegisterUserViewModel usuarioRegistro) { 
+            if(!ModelState.IsValid) return View(usuarioRegistro);
+
+            //API - Registro
+
+            
+            
+            if(false) return View(usuarioRegistro);
+
+            //Realizar login na app
+            return RedirectToAction("Index", controllerName: "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View();
+        }
+        
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginUserViewModel usarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usarioLogin);
+
+            //API - Registro
+
+
+
+            if (false) return View(usarioLogin);
+
+            //Realizar login na app
+            return RedirectToAction("Index", controllerName: "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
