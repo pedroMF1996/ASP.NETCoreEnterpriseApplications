b2498c55c5e18e9d3bc494b7b91573c249d80320
pmfrp <pmfrp@hotmail.com>
2023-09-19 18:18:47 -0300

    Alterando o layout do ecomerce

---
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Extensions/RazorHelpers.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public static class RazorHelpers
+    {
+        public static string HashEmailForGravatar(this RazorPage page, string email)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(email));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        public static string FormatoMoeda(this RazorPage page, decimal valor)
+        {
+            return valor > 0 ? string.Format(Thread.CurrentThread.CurrentCulture, "{0:C}", valor) : "Gratuito";
+        }
+
+        public static string MensagemEstoque(this RazorPage page, int quantidade)
+        {
+            return quantidade > 0 ? $"Apenas {quantidade} em estoque!" : "Produto esgotado!";
+        }
+    }
+}
