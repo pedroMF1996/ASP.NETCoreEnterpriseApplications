0377a5c5c7134fb622b6c2717b581d86d37039da
pmfrp <pmfrp@hotmail.com>
2023-09-20 17:29:41 -0300

    Api Catalogo Context, Repository, IUnityOfWork, AgregateRoot

---
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+
+namespace NSE.Catalogo.API.Data.Repository
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly CatalogoContext _context;
+        public IUnitOfWork unitOfWork => _context;
+
+        public ProdutoRepository(CatalogoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Produto> ObterPorId(int id)
+        {
+            return await _context.Produtos.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Produto>> ObterTodos()
+        {
+            return await _context.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        public void Adicionar(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+        }
+
+        public void Atualizar(Produto produto)
+        {
+            _context.Produtos.Update(produto);
+        }
+
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Program.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Data;
+using NSE.Catalogo.API.Data.Repository;
+using NSE.Catalogo.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<CatalogoContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IProdutoRepository, ProdutoRepository>();
+builder.Services.AddScoped<CatalogoContext>();
+
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
