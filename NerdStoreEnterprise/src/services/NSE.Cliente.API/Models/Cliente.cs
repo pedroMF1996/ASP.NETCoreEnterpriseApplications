c69b00f574f4108022db86ef3a0dbef475d07f03
pmfrp <pmfrp@hotmail.com>
2023-09-25 18:00:30 -0300

    Objetos de valor e validacoes

--- NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
+++ NerdStoreEnterprise/src/services/NSE.Cliente.API/Models/Cliente.cs
@@ -5,8 +5,8 @@ namespace NSE.Cliente.API.Models
     public class Cliente : Entity, IAggregateRoot
     {
         public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Excluido { get; private set; }
         public Endereco Endereco { get; protected set; }
 
@@ -14,12 +14,23 @@ namespace NSE.Cliente.API.Models
         protected Cliente()
         {}
 
-        public Cliente(string nome, string email, string cpf)
+        public Cliente(Guid id, string nome, string email, string cpf)
         {
+            Id = id;
             Nome = nome;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Excluido = false;
+        }
+
+        public void TrocarEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void AtribuirEndereco(Endereco endereco)
+        {
+            Endereco = endereco;
         }
     }
 }
