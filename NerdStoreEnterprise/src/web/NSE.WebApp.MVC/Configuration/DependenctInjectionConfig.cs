b17b2230a486a0271355fa8ac463d39884cf1543
pmfrp <pmfrp@hotmail.com>
2023-09-25 15:19:12 -0300

    Usando Refit para gerir o HttpService

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependenctInjectionConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/DependenctInjectionConfig.cs
@@ -7,13 +7,21 @@ namespace NSE.WebApp.MVC.Configuration
 {
     public static class DependenctInjectionConfig
     {
-        public static void AddRegisterServises(this IServiceCollection services)
+        public static void AddRegisterServises(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<HttpCientAuthorizationDelegateHandler>();
 
             services.AddHttpClient<IAutenticacaoService, AutenticacaoServise>();
-            services.AddHttpClient<ICatalogoService, CatalogoService>()
-                .AddHttpMessageHandler<HttpCientAuthorizationDelegateHandler>();
+
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //    .AddHttpMessageHandler<HttpCientAuthorizationDelegateHandler>();
+            
+            services.AddHttpClient("Refit", options =>
+                {
+                    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+                })
+                .AddHttpMessageHandler<HttpCientAuthorizationDelegateHandler>()
+                .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
