37b3e056975222d15aa8fe0108e0f9daa1aa1d4c
pmfrp <pmfrp@hotmail.com>
2023-09-27 14:29:22 -0300

    Savepoint

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -1,18 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.WebApp.MVC.Models
 {
     public class RegisterUserViewModel
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("Nome Completo")]
+        public string Nome {  get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DisplayName("CPF")]
+        public string Cpf {  get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+        [DisplayName("Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        [DisplayName("Senha")]
         public string Password { get; set; }
 
         [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+        [DisplayName("Senha")]
         public string ConfirmPassword { get; set; }
     }
 
