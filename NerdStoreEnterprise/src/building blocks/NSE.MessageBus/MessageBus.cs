296d2f8af5abf52bfe46d13b1332cbcdf77d4c0b
pmfrp <pmfrp@hotmail.com>
2023-09-28 16:34:47 -0300

    Iniciando a implementacao do MessageBus

---
+++ NerdStoreEnterprise/src/building blocks/NSE.MessageBus/MessageBus.cs
@@ -0,0 +1,102 @@
+﻿using EasyNetQ;
+using NSE.Core.Messages.Integration;
+using Polly;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private IBus _bus;
+
+        private readonly string _connectionString;
+
+        public MessageBus(string connectionString)
+        {
+            _connectionString = connectionString;
+            TryConnectRabbitMQ();
+        }
+
+        public bool IsConected => _bus?.Advanced.IsConnected ?? false;
+
+        public void Publish<T>(T message) where T : IntegrationEvent
+        {
+            TryConnectRabbitMQ();
+            _bus.PubSub.Publish(message);
+        }
+
+        public async Task PublishAsync<T>(T message) where T : IntegrationEvent
+        {
+            TryConnectRabbitMQ();
+            await _bus.PubSub.PublishAsync(message);
+        }
+
+        public TResponse Request<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnectRabbitMQ();
+            return _bus.Rpc.Request<TRequest, TResponse>(request);
+        }
+
+        public async Task<TResponse> RequestAsync<TRequest, TResponse>(TRequest request)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnectRabbitMQ();
+            return await _bus.Rpc.RequestAsync<TRequest, TResponse>(request);
+        }
+
+        public IDisposable Respond<TRequest, TResponse>(Func<TRequest, TResponse> responder)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnectRabbitMQ();
+            return _bus.Rpc.Respond(responder);
+        }
+
+        public async Task<IDisposable> RespondAsync<TRequest, TResponse>(Func<TRequest, Task<TResponse>> responder)
+            where TRequest : IntegrationEvent
+            where TResponse : ResponseMessage
+        {
+            TryConnectRabbitMQ();
+            return await _bus.Rpc.RespondAsync(responder);
+        }
+
+        public void Subscribe<T>(string subscriptionId, Action<T> onMessage) where T : class
+        {
+            TryConnectRabbitMQ();
+            _bus.PubSub.Subscribe(subscriptionId, onMessage);
+        }
+
+        public async Task SubscribeAsync<T>(string subscriptionId, Func<T, Task> onMessage) where T : class
+        {
+            await _bus.PubSub.SubscribeAsync(subscriptionId, onMessage);
+        }
+
+        private void TryConnectRabbitMQ()
+        {
+            if (IsConected)
+                return;
+
+            var policy = Policy.Handle<EasyNetQException>()
+                .Or<BrokerUnreachableException>()
+                .WaitAndRetry(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            policy.Execute(() =>
+            {
+                _bus = RabbitHutch.CreateBus(_connectionString);
+            });
+        }
+
+        public void Dispose()
+        {
+            _bus.Dispose();
+        }
+    }
+}
