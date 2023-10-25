3821569c9195e3ca123bd5a3b73bd34b9cf74860
Pedro Martins Falleiros <pmfrp@hotmail.com>
2023-10-25 18:28:27 -0300

    Gateway de pagamento

---
+++ NerdStoreEnterprise/src/services/NSE.Pagamento.API/Configurations/ApiConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using NSE.WebAPI.Core.Identidade;
+
+namespace NSE.Pagamento.API.Configurations
+{
+    public static class ApiConfiguration
+    {
+        public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSwaggerConfiguration();
+            services.AddJwtConfiguration(configuration);
+
+
+            services.AddCors(opt => 
+                opt.AddPolicy("total", builder => 
+                                        builder.AllowAnyHeader()
+                                                .AllowAnyMethod()   
+                                                .AllowAnyOrigin()));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment environment)
+        {
+            app.UseSwaggerConfiguration(environment);
+            app.UseCors("total");
+            return app;
+        }
+    }
+}
---
+++ NerdStoreEnterprise/src/services/NSE.Pagamento.API/Configurations/RegisterServicesConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Pagamento.API.Configurations
+{
+    public static class RegisterServicesConfiguration
+    {
+        public static IServiceCollection AddRegisterServisesConfiguration(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
