ac54bdef7c533baa6c8fe3b97874b3c91bc01267
Pedro Martins Falleiros <pmfrp@hotmail.com>
2023-11-02 11:43:21 -0300

    Processos de Captura e de cancelamento de pagamento

---
+++ NerdStoreEnterprise/src/building blocks/NSE.Core/Messages/Integration/PedidoPagoIntegrationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSE.Core.Messages.Integration
+{
+    public class PedidoPagoIntegrationEvent : IntegrationEvent
+    {
+        public PedidoPagoIntegrationEvent(Guid id, Guid clienteId)
+        {
+            Id = id;
+            ClienteId = clienteId;
+        }
+
+        public Guid Id { get; }
+        public Guid ClienteId { get; }
+    }
+}
