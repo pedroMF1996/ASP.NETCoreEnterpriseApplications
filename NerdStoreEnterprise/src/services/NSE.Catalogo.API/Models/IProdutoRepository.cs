c2ee03f714882c7788534bb459156364219c6c1c
Pedro Martins Falleiros <pmfrp@hotmail.com>
2023-11-02 16:32:55 -0300

    paginacao com dapper na API Catalogo no endpoint obter/produtos

--- NerdStoreEnterprise/src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
+++ NerdStoreEnterprise/src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
@@ -4,7 +4,7 @@ namespace NSE.Catalogo.API.Models
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
-        Task<IEnumerable<Produto>> ObterTodos();
+        Task<PagedResult<Produto>> ObterTodos(int pageSize, int pageIndex, string query);
         Task<Produto> ObterPorId(Guid id);
         void Adicionar(Produto produto);
         void Atualizar(Produto produto);
