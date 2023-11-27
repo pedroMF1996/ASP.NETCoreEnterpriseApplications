041a8296d13197a75a09a0aafe036c25826ba6f2
Pedro Martins Falleiros <pmfrp@hotmail.com>
2023-11-27 18:59:16 -0300

    Savepoints

--- NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ NerdStoreEnterprise/src/web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -16,18 +16,13 @@ namespace NSE.WebApp.MVC.Configuration
 
         public static void UseWebAppConfig(this IApplicationBuilder app, IWebHostEnvironment environment)
         {
-            // Configure the HTTP request pipeline.
-            //if (!environment.IsDevelopment())
-            //{
-
-            //}
-
             app.UseExceptionHandler("/erro/500");
             app.UseStatusCodePagesWithRedirects("/erro/{0}");
+            
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
